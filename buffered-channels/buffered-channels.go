20ad2e8bb03fb87ebfd87ba40dba1ae12ce2fad7
onurkybsi <onurbpm@outlook.com>
2020-09-20 23:25:45 +0300

    Exercise - 6

--- buffered-channels/buffered-channels.go
+++ buffered-channels/buffered-channels.go
@@ -1,23 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// By default channels are unbuffered,
-// meaning that they will only accept sends (chan <-)
-// if there is a corresponding receive (<- chan) ready to receive the sent value.
-// Buffered channels accept a limited number of values
-// without a corresponding receiver for those values.
-
-// Unbuffered channels block receivers until data is available on the channel and senders
-// until a receiver is available. Buffered channels will only block a sender once the buffer fills up.
+// Default unbuffered
 
 func main() {
-	// a channel of string buffering up to 2 values
-	message := make(chan string, 2)
+	channel := make(chan string, 1)
 
-	message <- "buffered"
-	message <- "channel"
+	// buffer = 1 so sender goroutine in this case following func doesnt block
+	// if we add channel <- "test2" to following func, buffer will be filled so func will be blocked
+	go func() {
+		channel <- "test"
 
-	fmt.Println(<-message)
-	fmt.Println(<-message)
+		fmt.Println("Send!")
+	}()
+
+	go func() {
+		time.Sleep(3 * time.Second)
+
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(4 * time.Second)
+	fmt.Println("Exited!")
 }
